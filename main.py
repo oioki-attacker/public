e17b706aaa0ff1bf5142af7307b9fd5ad6a495b2
Alexander Tarasov <alex.tarasov@sentry.io>
2024-01-08 12:14:48 +0100

    tests

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def core(a, b):
+    if b > 0:
+        return a / b;
+    return a / b;
+
+if __name__ == '__main__':
+    print(core(1, 4))
---
+++ test_main.py
@@ -0,0 +1,5 @@
+from main import core
+
+def test_core():
+    assert core(1, 4) == 0.25
+
